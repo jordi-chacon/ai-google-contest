d0a1dbdde2b642cb61c55e16c8c784c80135ceef
Jordi Chacon <jordi.chacon@klarna.com>
2010-11-03 14:49:44 +0100

    Improved bot by adding two new checks: 1. we don't go to neutral planets with more than 30 ships because we would be wasting those 30 ships, it's better if the opponent wastes his ships and then we go and take the planet. 2. we don't send more fleets to planets which we have fleets going to it at the moment and we are kind of sure that we will take it

--- MyBot.cc
+++ MyBot.cc
@@ -11,7 +11,28 @@ double compute_score(int growth_rate, int num_ships, int distance) {
   double growth_rate_score = 40 * growth_rate;
   double num_ships_score = 5000 / num_ships / 2;
   double distance_score = 1000 / distance;
+  ofstream myfile;
+  /*  myfile.open("output",ios::app);
+  myfile << growth_rate_score + num_ships_score + distance_score << " = " <<
+    growth_rate << "("<<growth_rate_score<<")   + " << num_ships << " (" <<
+    num_ships_score << ")   + " << distance << " (" << distance_score << ")\n";
+    myfile.close();*/
   return growth_rate_score + num_ships_score + distance_score;
+}
+
+bool is_enough_fleets_going_to_planet(vector<Fleet> fleets, Planet p) {
+  int fleet_sum = 0;
+  for(vector<Fleet>::const_iterator it = fleets.begin(); it < fleets.end(); ++it) {
+    if(it->DestinationPlanet() == p.PlanetID() && it->Owner() == 1)
+      fleet_sum += it->NumShips();
+    if(it->DestinationPlanet() == p.PlanetID() && it->Owner() == 2)
+      fleet_sum -= it->NumShips();
+  }
+  return fleet_sum > p.NumShips() + 5;
+}
+
+bool is_stupid_going_to_neutral_planet(Planet p) {
+  return p.Owner() == 0 && p.NumShips() > 30;
 }
 
 void SendShips(const PlanetWars& pw, int my_planet, int my_planet_ships) {
@@ -20,38 +41,41 @@ void SendShips(const PlanetWars& pw, int my_planet, int my_planet_ships) {
   vector<PlanetScore> not_my_planets_score;
   for(vector<Planet>::const_iterator it = not_my_planets.begin();
       it < not_my_planets.end(); ++it) {
-    int num_ships = it->NumShips();
-    int growth_rate = it->GrowthRate();
-    int distance = pw.Distance(it->PlanetID(), my_planet);
-    double score = compute_score(growth_rate, num_ships, distance);
-    PlanetScore* ps = new PlanetScore(*it, score);
-    not_my_planets_score.push_back(*ps);
+    if(!is_stupid_going_to_neutral_planet(*it)) {
+      int num_ships = it->NumShips();
+      int growth_rate = it->GrowthRate();
+      int distance = pw.Distance(it->PlanetID(), my_planet);
+      double score = compute_score(growth_rate, num_ships, distance);
+      PlanetScore* ps = new PlanetScore(*it, score);
+      not_my_planets_score.push_back(*ps);
+    }
   }
   sort(not_my_planets_score.begin(), not_my_planets_score.end());
 
   // (2) Send ships from my planet to the weakest planets that I don't own
+  // and that I haven't seen fleets to it
+  vector<Fleet> fleets = pw.Fleets();
   for(vector<PlanetScore>::iterator it = not_my_planets_score.begin();
       it < not_my_planets_score.end(); ++it) {
     Planet p = *(it->GetPlanet());
-    if(my_planet_ships > p.NumShips() + 5) {
-      pw.IssueOrder(my_planet, p.PlanetID(), p.NumShips()+1);
-      my_planet_ships -= p.NumShips();
+    if(!is_enough_fleets_going_to_planet(fleets, p)) {
+      if(my_planet_ships > p.NumShips() + 5) {
+	pw.IssueOrder(my_planet, p.PlanetID(), p.NumShips()+1);
+	my_planet_ships -= p.NumShips();
+      }
+      else break;
     }
-    else break;
   }
 }
 
 void DoTurn(const PlanetWars& pw, int turn) {
   // Per each of my planets try to send ships to other planets
-  //ofstream myfile;
-  //myfile.open("output",ios::app);
   int strongest_planet;
   int strongest_planet_ships;
   vector<Planet> my_planets = pw.MyPlanets();
   for (int i = 0; i < my_planets.size(); ++i) {
     SendShips(pw, my_planets[i].PlanetID(), my_planets[i].NumShips());
   }
-  //myfile.close();
 }
 
 
@@ -61,7 +85,7 @@ int main(int argc, char *argv[]) {
   std::string current_line;
   std::string map_data;
   int turn = 0;
-  ofstream myfile;
+  //  ofstream myfile;
   //  myfile.open("output");
   //  myfile.close();
   while (true) {
