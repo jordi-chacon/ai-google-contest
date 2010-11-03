a299c6f3de9ee4c70fd3b1831e1539eae65f8f5d
Jordi Chacon <jordi.chacon@klarna.com>
2010-11-03 13:16:41 +0100

    Refactored code

--- MyBot.cc
+++ MyBot.cc
@@ -5,47 +5,53 @@
 #include "PlanetScore.h"
 using namespace std;
 
-void SendShips(const PlanetWars& pw, int my_planet, int my_planet_ships) {
-  //  ofstream myfile;
-  // myfile.open("output", ios::app);
+double compute_score(int growth_rate, int num_ships, int distance) {
+  if(num_ships == 0) num_ships = 1;
+  if(distance == 0) distance = 1;
+  double growth_rate_score = 40 * growth_rate;
+  double num_ships_score = 5000 / num_ships / 2;
+  double distance_score = 1000 / distance;
+  return growth_rate_score + num_ships_score + distance_score;
+}
 
+void SendShips(const PlanetWars& pw, int my_planet, int my_planet_ships) {
   // (1) Find most interesting planets to send fleets
-  std::vector<Planet> neutral_planets = pw.NotMyPlanets();
-  std::vector<PlanetScore> neutral_planets_score;
-  for(std::vector<Planet>::const_iterator it = neutral_planets.begin();
-      it < neutral_planets.end(); ++it) {
+  vector<Planet> not_my_planets = pw.NotMyPlanets();
+  vector<PlanetScore> not_my_planets_score;
+  for(vector<Planet>::const_iterator it = not_my_planets.begin();
+      it < not_my_planets.end(); ++it) {
     int num_ships = it->NumShips();
     int growth_rate = it->GrowthRate();
     int distance = pw.Distance(it->PlanetID(), my_planet);
-    double score = 40*growth_rate + 5000/num_ships/2 + 1000/distance;
-    //    myfile << score << " = " << growth_rate << "(" << 40*growth_rate<<") + "<<
-    //  num_ships<<"("<<5000/num_ships/2<<") + "<<distance<<"("<<1000/distance<<")\n";
+    double score = compute_score(growth_rate, num_ships, distance);
     PlanetScore* ps = new PlanetScore(*it, score);
-    neutral_planets_score.push_back(*ps);
+    not_my_planets_score.push_back(*ps);
   }
-  sort(neutral_planets_score.begin(), neutral_planets_score.end());
+  sort(not_my_planets_score.begin(), not_my_planets_score.end());
 
   // (2) Send ships from my planet to the weakest planets that I don't own
-  for(std::vector<PlanetScore>::iterator it = neutral_planets_score.begin();
-      it < neutral_planets_score.end(); ++it) {
+  for(vector<PlanetScore>::iterator it = not_my_planets_score.begin();
+      it < not_my_planets_score.end(); ++it) {
     Planet p = *(it->GetPlanet());
     if(my_planet_ships > p.NumShips() + 5) {
       pw.IssueOrder(my_planet, p.PlanetID(), p.NumShips()+1);
       my_planet_ships -= p.NumShips();
     }
-    else return;
+    else break;
   }
 }
 
-void DoTurn(const PlanetWars& pw) {
+void DoTurn(const PlanetWars& pw, int turn) {
   // Per each of my planets try to send ships to other planets
-  int strongest_planet = -1;
-  double source_score = -999999.0;
-  int strongest_planet_ships = 0;
-  std::vector<Planet> my_planets = pw.MyPlanets();
+  //ofstream myfile;
+  //myfile.open("output",ios::app);
+  int strongest_planet;
+  int strongest_planet_ships;
+  vector<Planet> my_planets = pw.MyPlanets();
   for (int i = 0; i < my_planets.size(); ++i) {
     SendShips(pw, my_planets[i].PlanetID(), my_planets[i].NumShips());
   }
+  //myfile.close();
 }
 
 
@@ -54,14 +60,19 @@ void DoTurn(const PlanetWars& pw) {
 int main(int argc, char *argv[]) {
   std::string current_line;
   std::string map_data;
+  int turn = 0;
+  ofstream myfile;
+  //  myfile.open("output");
+  //  myfile.close();
   while (true) {
     int c = std::cin.get();
     current_line += (char)c;
     if (c == '\n') {
       if (current_line.length() >= 2 && current_line.substr(0, 2) == "go") {
+	turn++;
         PlanetWars pw(map_data);
         map_data = "";
-        DoTurn(pw);
+        DoTurn(pw, turn);
 	pw.FinishTurn();
       } else {
         map_data += current_line;
