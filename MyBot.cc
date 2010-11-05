20877d795598e783052f8ab34598f2c9de154274
Jordi Chacon <jordi.chacon@klarna.com>
2010-11-05 12:08:11 +0100

    PlanetWars pw variable is global now, since it was being sent through almost all the funcions in the code

--- MyBot.cc
+++ MyBot.cc
@@ -5,6 +5,8 @@
 #include "PlanetScore.h"
 using namespace std;
 
+PlanetWars* pw;
+
 double compute_score(int growth_rate, int num_ships, int distance, bool is_enemy) {
   if(num_ships == 0) num_ships = 1;
   if(distance == 0) distance = 1;
@@ -36,20 +38,20 @@ bool is_neutral_planet_with_many_fleets(Planet p) {
   return p.Owner() == 0 && p.NumShips() > 30;
 }
 
-bool is_stupid_to_go_to_planet(const PlanetWars& pw, Planet p, int turn) {
+bool is_stupid_to_go_to_planet(Planet p, int turn) {
   return is_neutral_planet_with_many_fleets(p);
 }
 
-void SendShips(const PlanetWars& pw, int my_planet, int my_planet_ships, int turn) {
+void SendShips(int my_planet, int my_planet_ships, int turn) {
   // (1) Find most interesting planets to send fleets
-  vector<Planet> not_my_planets = pw.NotMyPlanets();
+  vector<Planet> not_my_planets = pw->NotMyPlanets();
   vector<PlanetScore> not_my_planets_score;
   for(vector<Planet>::const_iterator it = not_my_planets.begin();
       it < not_my_planets.end(); ++it) {
-    if(!is_stupid_to_go_to_planet(pw, *it, turn)) {
+    if(!is_stupid_to_go_to_planet(*it, turn)) {
       int num_ships = it->NumShips();
       int growth_rate = it->GrowthRate();
-      int distance = pw.Distance(it->PlanetID(), my_planet);
+      int distance = pw->Distance(it->PlanetID(), my_planet);
       double score = compute_score(growth_rate, num_ships, distance,
 				   it->Owner() == 2);
       PlanetScore* ps = new PlanetScore(*it, score);
@@ -60,17 +62,17 @@ void SendShips(const PlanetWars& pw, int my_planet, int my_planet_ships, int tur
 
   // (2) Send ships from my planet to the weakest planets that I don't own
   // and that I haven't seen fleets to it
-  vector<Fleet> fleets = pw.Fleets();
+  vector<Fleet> fleets = pw->Fleets();
   for(vector<PlanetScore>::iterator it = not_my_planets_score.begin();
       it < not_my_planets_score.end(); ++it) {
     Planet p = *(it->GetPlanet());
     if(!is_enough_fleets_going_to_planet(fleets, p)) {
       if(p.GrowthRate() == 5 && my_planet_ships - 5 > p.NumShips()) {
-	pw.IssueOrder(my_planet, p.PlanetID(), my_planet_ships - 5);
+	pw->IssueOrder(my_planet, p.PlanetID(), my_planet_ships - 5);
 	my_planet_ships = 5;
       }
       else if(my_planet_ships > p.NumShips()*2 + 5) {
-	pw.IssueOrder(my_planet, p.PlanetID(), p.NumShips()*2);
+	pw->IssueOrder(my_planet, p.PlanetID(), p.NumShips()*2);
 	my_planet_ships -= p.NumShips()*2;
       }
       else break;
@@ -78,8 +80,8 @@ void SendShips(const PlanetWars& pw, int my_planet, int my_planet_ships, int tur
   }
 }
 
-int ships_needed_to_defend_planet(const PlanetWars& pw, Planet p) {
-  vector<Fleet> enemy_fleets = pw.EnemyFleets();
+int ships_needed_to_defend_planet(Planet p) {
+  vector<Fleet> enemy_fleets = pw->EnemyFleets();
   vector<int> ships_available_per_turn(50);
   ships_available_per_turn[0] = p.NumShips();
   for(int i = 1; i < 50; i++) {
@@ -102,14 +104,14 @@ int ships_needed_to_defend_planet(const PlanetWars& pw, Planet p) {
   else return minimum_ships_in_planet_for_next_turns;
 }
 
-void DoTurn(const PlanetWars& pw, int turn) {
+void DoTurn(int turn) {
   // Per each of my planets try to send ships to other planets
   int strongest_planet;
   int strongest_planet_ships;
-  vector<Planet> my_planets = pw.MyPlanets();
+  vector<Planet> my_planets = pw->MyPlanets();
   for (int i = 0; i < my_planets.size(); ++i) {
-    int ships_needed_to_defend = ships_needed_to_defend_planet(pw, my_planets[i]);
-    SendShips(pw, my_planets[i].PlanetID(), ships_needed_to_defend, turn);
+    int ships_needed_to_defend = ships_needed_to_defend_planet(my_planets[i]);
+    SendShips(my_planets[i].PlanetID(), ships_needed_to_defend, turn);
   }
 }
 
@@ -129,10 +131,11 @@ int main(int argc, char *argv[]) {
     if (c == '\n') {
       if (current_line.length() >= 2 && current_line.substr(0, 2) == "go") {
 	turn++;
-        PlanetWars pw(map_data);
+        PlanetWars planet_wars(map_data);
+	pw = &planet_wars;
         map_data = "";
-        DoTurn(pw, turn);
-	pw.FinishTurn();
+        DoTurn(turn);
+	pw->FinishTurn();
       } else {
         map_data += current_line;
       }
