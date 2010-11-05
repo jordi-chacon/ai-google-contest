ee67f9958a690242419f639d0b089c485fe15480
Jordi Chacon <jordi.chacon@klarna.com>
2010-11-05 12:54:23 +0100

    Moved functions so that now the code can be read from top to bottom. Looks much better

--- MyBot.cc
+++ MyBot.cc
@@ -7,39 +7,90 @@ using namespace std;
 
 PlanetWars* pw;
 
-double compute_score(int growth_rate, int num_ships, int distance, bool is_enemy) {
-  if(num_ships == 0) num_ships = 1;
-  if(distance == 0) distance = 1;
-  double growth_rate_score = 60 * growth_rate;
-  double num_ships_score = 5000 / num_ships / 2;
-  double distance_score = 1000 / distance;
-  double enemy_bonus = 0;
-  /*  ofstream myfile;
-  myfile.open("output",ios::app);
-  myfile << growth_rate_score + num_ships_score + distance_score << " = " <<
-    growth_rate << "("<<growth_rate_score<<")   + " << num_ships << " (" <<
-    num_ships_score << ")   + " << distance << " (" << distance_score << ")\n";
-    myfile.close();*/
-  return growth_rate_score + num_ships_score + distance_score + enemy_bonus;
-}
+double compute_score(int growth_rate, int num_ships, int distance, bool is_enemy);
+bool is_enough_fleets_going_to_planet(vector<Fleet> fleets, Planet p);
+bool is_neutral_planet_with_many_fleets(Planet p);
+bool is_stupid_to_go_to_planet(Planet p, int turn);
+void send_ships(int my_planet, int my_planet_ships, int turn);
+vector<int> compute_ships_available_per_turn_before_attacks(Planet p);
+vector<int> compute_ships_available_per_turn_with_attacks(vector<int> ships_per_turn, Planet p);
+int ships_needed_to_keep_planet(Planet p);
+void try_attack_planets(int turn);
+void DoTurn(int turn);
 
-bool is_enough_fleets_going_to_planet(vector<Fleet> fleets, Planet p) {
-  int fleet_sum = 0;
-  for(vector<Fleet>::const_iterator it = fleets.begin(); it < fleets.end(); ++it) {
-    if(it->DestinationPlanet() == p.PlanetID() && it->Owner() == 1)
-      fleet_sum += it->NumShips();
-    if(it->DestinationPlanet() == p.PlanetID() && it->Owner() == 2)
-      fleet_sum -= it->NumShips();
+
+int main(int argc, char *argv[]) {
+  std::string current_line;
+  std::string map_data;
+  int turn = 0;
+  //   ofstream myfile;
+  //   myfile.open("output");
+  //   myfile.close();
+  while (true) {
+    int c = std::cin.get();
+    current_line += (char)c;
+    if (c == '\n') {
+      if (current_line.length() >= 2 && current_line.substr(0, 2) == "go") {
+	turn++;
+        PlanetWars planet_wars(map_data);
+	pw = &planet_wars;
+        map_data = "";
+        DoTurn(turn);
+	pw->FinishTurn();
+      } else {
+        map_data += current_line;
+      }
+      current_line = "";
+    }
   }
-  return fleet_sum > p.NumShips() + 5;
+  return 0;
 }
 
-bool is_neutral_planet_with_many_fleets(Planet p) {
-  return p.Owner() == 0 && p.NumShips() > 30;
+void DoTurn(int turn) {
+  try_attack_planets(turn);
 }
 
-bool is_stupid_to_go_to_planet(Planet p, int turn) {
-  return is_neutral_planet_with_many_fleets(p);
+void try_attack_planets(int turn) {
+  int strongest_planet;
+  int strongest_planet_ships;
+  vector<Planet> my_planets = pw->MyPlanets();
+  for (int i = 0; i < my_planets.size(); ++i) {
+    int ships_needed_to_keep = ships_needed_to_keep_planet(my_planets[i]);
+    send_ships(my_planets[i].PlanetID(), ships_needed_to_keep, turn);
+  }
+}
+
+int ships_needed_to_keep_planet(Planet p) {
+  vector<int> ships_per_turn = compute_ships_available_per_turn_before_attacks(p);
+  ships_per_turn = compute_ships_available_per_turn_with_attacks(ships_per_turn, p);
+  int minimum_ships_in_planet_for_next_turns = 99999;
+  for(int i = 1; i < 50; i++) {
+    if(ships_per_turn[i] < minimum_ships_in_planet_for_next_turns)
+      minimum_ships_in_planet_for_next_turns = ships_per_turn[i];
+  }
+  if(minimum_ships_in_planet_for_next_turns < 0) return 0;
+  else return minimum_ships_in_planet_for_next_turns;
+}
+
+vector<int> compute_ships_available_per_turn_with_attacks(vector<int> ships_per_turn, Planet p) {
+  vector<Fleet> enemy_fleets = pw->EnemyFleets();
+  for(vector<Fleet>::iterator it = enemy_fleets.begin(); it < enemy_fleets.end(); ++it) {
+    if(it->DestinationPlanet() == p.PlanetID())
+      ships_per_turn[it->TurnsRemaining()] -= it->NumShips();
+  }
+  for(int i = 1; i < 50; i++) {
+    ships_per_turn[i] -= ships_per_turn[i-1];
+  }
+  return ships_per_turn;
+}
+
+vector<int> compute_ships_available_per_turn_before_attacks(Planet p) {
+  vector<int> ships_available_per_turn(50);
+  ships_available_per_turn[0] = p.NumShips();
+  for(int i = 1; i < 50; i++) {
+    ships_available_per_turn[i] = ships_available_per_turn[i-1] + p.NumShips();
+  }
+  return ships_available_per_turn;
 }
 
 void send_ships(int my_planet, int my_planet_ships, int turn) {
@@ -80,80 +131,37 @@ void send_ships(int my_planet, int my_planet_ships, int turn) {
   }
 }
 
-vector<int> compute_ships_available_per_turn_before_attacks(Planet p) {
-  vector<int> ships_available_per_turn(50);
-  ships_available_per_turn[0] = p.NumShips();
-  for(int i = 1; i < 50; i++) {
-    ships_available_per_turn[i] = ships_available_per_turn[i-1] + p.NumShips();
+bool is_enough_fleets_going_to_planet(vector<Fleet> fleets, Planet p) {
+  int fleet_sum = 0;
+  for(vector<Fleet>::const_iterator it = fleets.begin(); it < fleets.end(); ++it) {
+    if(it->DestinationPlanet() == p.PlanetID() && it->Owner() == 1)
+      fleet_sum += it->NumShips();
+    if(it->DestinationPlanet() == p.PlanetID() && it->Owner() == 2)
+      fleet_sum -= it->NumShips();
   }
-  return ships_available_per_turn;
+  return fleet_sum > p.NumShips() + 5;
 }
 
-vector<int> compute_ships_available_per_turn_with_attacks(vector<int> ships_per_turn,
-							  Planet p) {
-  vector<Fleet> enemy_fleets = pw->EnemyFleets();
-  for(vector<Fleet>::iterator it = enemy_fleets.begin(); it < enemy_fleets.end(); ++it) {
-    if(it->DestinationPlanet() == p.PlanetID())
-      ships_per_turn[it->TurnsRemaining()] -= it->NumShips();
-  }
-  for(int i = 1; i < 50; i++) {
-    ships_per_turn[i] -= ships_per_turn[i-1];
-  }
-  return ships_per_turn;
+bool is_stupid_to_go_to_planet(Planet p, int turn) {
+  return is_neutral_planet_with_many_fleets(p);
 }
 
-int ships_needed_to_keep_planet(Planet p) {
-  vector<int> ships_per_turn = compute_ships_available_per_turn_before_attacks(p);
-  ships_per_turn = compute_ships_available_per_turn_with_attacks(ships_per_turn, p);
-  int minimum_ships_in_planet_for_next_turns = 99999;
-  for(int i = 1; i < 50; i++) {
-    if(ships_per_turn[i] < minimum_ships_in_planet_for_next_turns)
-      minimum_ships_in_planet_for_next_turns = ships_per_turn[i];
-  }
-  if(minimum_ships_in_planet_for_next_turns < 0) return 0;
-  else return minimum_ships_in_planet_for_next_turns;
+bool is_neutral_planet_with_many_fleets(Planet p) {
+  return p.Owner() == 0 && p.NumShips() > 30;
 }
 
-void try_attack_planets(int turn) {
-  int strongest_planet;
-  int strongest_planet_ships;
-  vector<Planet> my_planets = pw->MyPlanets();
-  for (int i = 0; i < my_planets.size(); ++i) {
-    int ships_needed_to_keep = ships_needed_to_keep_planet(my_planets[i]);
-    send_ships(my_planets[i].PlanetID(), ships_needed_to_keep, turn);
-  }
-}
-
-void DoTurn(int turn) {
-  try_attack_planets(turn);
-}
-
-
-// This is just the main game loop that takes care of communicating with the
-// game engine for you. You don't have to understand or change the code below.
-int main(int argc, char *argv[]) {
-  std::string current_line;
-  std::string map_data;
-  int turn = 0;
-  //   ofstream myfile;
-  //   myfile.open("output");
-  //   myfile.close();
-  while (true) {
-    int c = std::cin.get();
-    current_line += (char)c;
-    if (c == '\n') {
-      if (current_line.length() >= 2 && current_line.substr(0, 2) == "go") {
-	turn++;
-        PlanetWars planet_wars(map_data);
-	pw = &planet_wars;
-        map_data = "";
-        DoTurn(turn);
-	pw->FinishTurn();
-      } else {
-        map_data += current_line;
-      }
-      current_line = "";
-    }
-  }
-  return 0;
+double compute_score(int growth_rate, int num_ships, int distance, bool is_enemy) {
+  if(num_ships == 0) num_ships = 1;
+  if(distance == 0) distance = 1;
+  double growth_rate_score = 60 * growth_rate;
+  double num_ships_score = 5000 / num_ships / 2;
+  double distance_score = 1000 / distance;
+  double enemy_bonus = 0;
+  /*  ofstream myfile;
+  myfile.open("output",ios::app);
+  myfile << growth_rate_score + num_ships_score + distance_score << " = " <<
+    growth_rate << "("<<growth_rate_score<<")   + " << num_ships << " (" <<
+    num_ships_score << ")   + " << distance << " (" << distance_score << ")\n";
+    myfile.close();*/
+  return growth_rate_score + num_ships_score + distance_score + enemy_bonus;
 }
